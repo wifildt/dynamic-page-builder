91c89b0e6b2a351fe05faecae98f41f228d8cb58
Lê Danh Trọng <trongld@nexusti.vn>
2024-01-07 02:27:57 +0700

    setup eslint & prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,18 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
+    'airbnb',
+    'airbnb-typescript'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+  },
+  settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
